3f7633f9500081def3ed0f26322a4a343f41f7ca
Joe Moorhouse & Kavya R <pair+jbhouse.krammohan@devbootcamp.com>
2017-03-17 14:39:06 -0700

    added upvote and downvote buttons with helper methods

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -12,3 +12,19 @@ post '/answers/:answer_id/downvote' do
   p @new_vote
   redirect "/questions/#{@answer.question_id}/answers"
 end
+
+
+post '/questions/:question_id/upvote' do
+  @new_vote = Vote.create(upvote: true, voteable_id: params[:question_id], voteable_type: "Question")
+  @question = Question.find(params[:question_id])
+  p @new_vote
+  redirect "/questions/#{@question.id}/answers"
+end
+
+
+post '/questions/:question_id/downvote' do
+  @new_vote = Vote.create(upvote: false, voteable_id: params[:question_id], voteable_type: "Question")
+  @question = Question.find(params[:question_id])
+  p @new_vote
+  redirect "/questions/#{@question.id}/answers"
+end
