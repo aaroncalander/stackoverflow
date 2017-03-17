f6f4f3306b56c58aeea856793a69941c5068db4b
Joe Moorhouse & Kavya R <pair+jbhouse.krammohan@devbootcamp.com>
2017-03-17 14:22:43 -0700

    created upvote and downvotes buttons with their post routes

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,14 @@
+post '/answers/:answer_id/upvote' do
+  @new_vote = Vote.create(upvote: true, voteable_id: params[:answer_id], voteable_type: "Answer")
+  @answer = Answer.find(params[:answer_id])
+  p @new_vote
+  redirect "/questions/#{@answer.question_id}/answers"
+end
+
+
+post '/answers/:answer_id/downvote' do
+  @new_vote = Vote.create(upvote: false, voteable_id: params[:answer_id], voteable_type: "Answer")
+  @answer = Answer.find(params[:answer_id])
+  p @new_vote
+  redirect "/questions/#{@answer.question_id}/answers"
+end
