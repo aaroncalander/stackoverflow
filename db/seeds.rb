f4eea5258892c8e39d467b1f59a253d0fb1c7a6f
Benjamin Acevedo & Ravi Joshi <pair+bacevedo01.ravibkjoshi@devbootcamp.com>
2017-03-17 11:52:22 -0700

    Fixes minor seed bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,27 +15,27 @@ end
 
 questions = 13.times.map do
   Question.create!( :content => Faker::Hacker.say_something_smart + "?",
-                    :user_id => rand(1..10)
+                    :user => User.all.sample
                     )
 end
 
 answers = 25.times.map do
   Answer.create!(:content => Faker::Hipster.paragraph,
-                :user_id => rand(1..10),
+                :user => User.all.sample,
                 :question => Question.all.sample
                   )
 end
 
 comments_on_questions = 10.times.map do
   Comment.create!(:content => Faker::Hipster.sentence,
-                  :user_id => rand(1..10),
+                  :user => User.all.sample,
                   :commentable => Question.all.sample
                   )
 end
 
 comments_on_answers = 16.times.map do
   Comment.create!(:content => Faker::Hipster.sentence,
-                  :user_id => rand(1..10),
+                  :user => User.all.sample,
                   :commentable => Answer.all.sample
                   )
 end
